3e8e5e921757720dcb03c72dbd49a287b1401809
yuppaoh <yuppaoh@gmail.com>
2019-07-26 00:42:43 +0700

    modify path in login.html.twig backend & frontend

---
+++ frontend/pages/aboutus.php
@@ -0,0 +1,11 @@
+<?php
+// Include file cấu hình ban đầu của `Twig`
+require_once __DIR__.'/../../bootstrap.php';
+
+// Truy vấn database
+// 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
+include_once(__DIR__.'/../../dbconnect.php');
+
+
+
+echo $twig->render('frontend/pages/aboutus.html.twig' );
