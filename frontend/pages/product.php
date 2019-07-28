e8d116f003211b4e4a9067bfea05bc3b6c695330
yuppaoh <yuppaoh@gmail.com>
2019-07-28 16:29:10 +0700

    modify product page on frontend

--- frontend/pages/product.php
+++ frontend/pages/product.php
@@ -12,12 +12,18 @@ $sql = <<<EOT
     SELECT sp.*
         , lsp.lsp_ten
         , nsx.nsx_ten
-        , km.km_ten, km.km_noidung, km.km_tungay, km.km_denngay
+        , km.km_ten, km.km_noidung, km.km_tungay, km.km_denngay, hsp.hsp_tentaptin
     FROM `sanpham` sp
     JOIN `loaisanpham` lsp ON sp.lsp_ma = lsp.lsp_ma
     JOIN `nhasanxuat` nsx ON sp.nsx_ma = nsx.nsx_ma
+    JOIN `hinhsanpham` hsp ON sp.sp_ma = hsp.sp_ma
     LEFT JOIN `khuyenmai` km ON sp.km_ma = km.km_ma
     ORDER BY sp.sp_ma DESC
+EOT;
+$sql_a = <<<EOT
+    SELECT *
+    FROM `hinhsanpham` hsp
+    JOIN `sanpham` sp on hsp.sp_ma = sp.sp_ma
 EOT;
 
 // 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
@@ -28,7 +34,7 @@ $result = mysqli_query($conn, $sql);
 $data = [];
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
 {
-    $km_tomtat = '';
+ /*   $km_tomtat = '';
     if(!empty($row['km_ten'])) {
         // Sử dụng hàm sprintf() để chuẩn bị mẫu câu với các giá trị truyền vào tương ứng từng vị trí placeholder
         $km_tomtat = sprintf("Khuyến mãi %s, nội dung: %s, thời gian: %s-%s", 
@@ -38,26 +44,26 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
             // Do hàm date() nhận vào là đối tượng thời gian, chúng ta cần sử dụng hàm strtotime() để chuyển đổi từ chuỗi có định dạng 'yyyy-mm-dd' trong MYSQL thành đối tượng ngày tháng
             date('d/m/Y', strtotime($row['km_tungay'])),    //vd: '2019-04-25'
             date('d/m/Y', strtotime($row['km_denngay'])));  //vd: '2019-05-10'
-    }
+    }*/
     $data[] = array(
+        'hsp_tentaptin' => $row['hsp_tentaptin'],
+        'sp_gia' => number_format($row['sp_gia'], 0, ".", ",") . ' vnđ',
+        'sp_ten' => $row['sp_ten'],   
+
         'sp_ma' => $row['sp_ma'],
-        'sp_ten' => $row['sp_ten'],
-        // Sử dụng hàm number_format(số tiền, số lẻ thập phân, dấu phân cách số lẻ, dấu phân cách hàng nghìn) để định dạng số khi hiển thị trên giao diện. Vd: 15800000 -> format thành 15,800,000.66 vnđ
-        'sp_gia' => number_format($row['sp_gia'], 2, ".", ",") . ' vnđ',
-        'sp_giacu' => number_format($row['sp_giacu'], 2, ".", ",") . ' vnđ',
+        
         'sp_mota_ngan' => $row['sp_mota_ngan'],
         'sp_mota_chitiet' => $row['sp_mota_chitiet'],
-        'sp_ngaycapnhat' => date('d/m/Y H:i:s', strtotime($row['sp_ngaycapnhat'])),
-        'sp_soluong' => number_format($row['sp_soluong'], 0, ".", ","),
-        'lsp_ma' => $row['lsp_ma'],
-        'nsx_ma' => $row['nsx_ma'],
+   
         'km_ma' => $row['km_ma'],
         // Các cột dữ liệu lấy từ liên kết khóa ngoại
+
         'lsp_ten' => $row['lsp_ten'],
-        'nsx_ten' => $row['nsx_ten'],
-        'km_tomtat' => $km_tomtat,
+        // 'nsx_ten' => $row['nsx_ten'],
+        // 'km_tomtat' => $km_tomtat,
+        
     );
 }
 // Yêu cầu `Twig` vẽ giao diện được viết trong file `backend/sanpham/index.html.twig`
 // với dữ liệu truyền vào file giao diện được đặt tên là `ds_sanpham`
-echo $twig->render('frontend/pages/product.html.twig', ['ds_sanpham' => $data] );
+echo $twig->render('frontend/pages/product.html.twig', ['ds_hinhsanpham' => $data] );
